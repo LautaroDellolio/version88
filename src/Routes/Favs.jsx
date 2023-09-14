fecffed4958db44a348c8631c4e70d9e6799c749
LautaroDellolio <lautarodellolio@gmail.com>
2023-09-13 21:08:05 -0300

    eliminarDelLocal

--- src/Routes/Favs.jsx
+++ src/Routes/Favs.jsx
@@ -5,23 +5,29 @@ import { ThemeContext } from "../Components/utils/global.context"
 const Favs = () => {
   const { state } = useContext(ThemeContext);
   const localFavorites = JSON.parse(localStorage.getItem("favorites"))
-  
+
   return (
 
     <div style={{ background: state.theme.background, color: state.theme.color }}>
       <div className='cardContainer'>
-        {localFavorites.map((favorite, index) => {
-          const parsedFavorite = JSON.parse(favorite);
-          return (
-            <div key={parsedFavorite.id} className='card'>
-              <img src="public/images/doctor.jpg" alt="Photo" />
-              <h2>{parsedFavorite.name}</h2>
-              <p>Email: {parsedFavorite.email}</p>
-              <p>Phone: {parsedFavorite.phone}</p>
-            </div>
-          );
-        })}
+        {localFavorites === null ? (
+          <h2>No hay favoritos</h2>
+        ) : (
+          localFavorites.map((favorite, index) => {
+            const parsedFavorite = JSON.parse(favorite);
+            return (
+              <div key={parsedFavorite.id} className='card'>
+                <img src="public/images/doctor.jpg" alt="Photo" />
+                <h2>{parsedFavorite.name}</h2>
+                <p>Email: {parsedFavorite.email}</p>
+                <p>Phone: {parsedFavorite.phone}</p>
+              </div>
+            );
+          })
+        )
+        }
       </div>
+      <button onClick={()=>localStorage.clear()}>Eliminar todo</button>
     </div>
   )
 }
@@ -29,6 +35,6 @@ const Favs = () => {
 export default Favs
 
 
-const handle = () =>{
+const handle = () => {
   console.log(local);
 }
